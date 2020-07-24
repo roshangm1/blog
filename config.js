fecabd8b0e0ea95403fb552a803dba6f8be03bbc
Roshan Gautam <roosan.gm@gmail.com>
2020-07-24 10:08:32 +0545

    remove yarn lint in ci

--- config.js
+++ config.js
@@ -1,46 +1,47 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://roshan.info.np',
-  pathPrefix: '/',
-  title: 'Blog by Roshan Gautam',
-  subtitle: 'Software Engineer & Co-founder at Brainants Technology. Expertise is React, React Native. Enthusiasts in backends - Servers, Machine Learning.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://roshan.info.np",
+  pathPrefix: "/",
+  title: "Blog by Roshan Gautam",
+  subtitle:
+    "Software Engineer & Co-founder at Brainants Technology. Expertise is React, React Native. Enthusiasts in backends - Servers, Machine Learning.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: '',
+  googleAnalyticsId: "",
   useKatex: false,
   menu: [
     {
-      label: 'Blogs',
-      path: '/'
+      label: "Blogs",
+      path: "/",
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about",
     },
-  
   ],
   author: {
-    name: 'Roshan Gautam  ',
-    photo: '/photo.jpg',
-    bio: 'Software Engineer & Co-founder at Brainants Technology. Expertise is React, React Native. Enthusiasts in server, machine learning.',
+    name: "Roshan Gautam  ",
+    photo: "/photo.jpg",
+    bio:
+      "Software Engineer & Co-founder at Brainants Technology. Expertise is React, React Native. Enthusiasts in server, machine learning.",
     contacts: {
-      email: 'roosan.gm@gmail.com',
-      facebook: '',
-      telegram: 'roshangm1',
-      twitter: 'roshangautam0',
-      github: 'roshangm1',
-      rss: '',
-      vkontakte: '',
-      linkedin: 'gautamroshan',
-      instagram: '',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+      email: "roosan.gm@gmail.com",
+      facebook: "",
+      telegram: "roshangm1",
+      twitter: "roshangautam0",
+      github: "roshangm1",
+      rss: "",
+      vkontakte: "",
+      linkedin: "gautamroshan",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
 };
