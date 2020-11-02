2807345592f1ed51d21e8c4afce9cdf48bd7fd73
Roshan Gautam <roosan.gm@gmail.com>
2020-11-02 20:38:16 +0545

    Darkmode switch in detail page

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -17,6 +17,7 @@ const Page = ({ title, children }: Props) => {
     <div ref={pageRef} className={styles["page"]}>
       <div className={styles["page__inner"]}>
         {title && <h1 className={styles["page__title"]}>{title}</h1>}
+
         <div className={styles["page__body"]}>{children}</div>
       </div>
     </div>
